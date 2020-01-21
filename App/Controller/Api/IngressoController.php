927812cfd592392cf5debb0d246ae4ea973a43c6
Lucas <lucas.dias@unyleya.com.br>
2020-01-21 20:09:47 -0300

    Método para atualização de informações de ingresso

--- App/Controller/Api/IngressoController.php
+++ App/Controller/Api/IngressoController.php
@@ -4,10 +4,11 @@
 namespace App\Controller\Api;
 
 
+use App\Controller\Controller;
 use App\Model\Entity\Botao;
 use Exception;
 
-class IngressoController
+class IngressoController extends Controller
 {
 
     /**
@@ -18,6 +19,21 @@ class IngressoController
     {
         $botao = new Botao();
         return $botao->getFirst($botao->findAll());
+    }
+
+    /**
+     * @return Botao
+     * @throws Exception
+     */
+    public function putAction()
+    {
+        $botao = new Botao();
+        $botao->setBlAtivo($this->request->getParameter("bl_ativo"));
+        $botao->setStLink($this->request->getParameter("st_link"));
+        $botao->setIdBotao(1);
+        $botao->save();
+        return $botao;
+
     }
 
 }
