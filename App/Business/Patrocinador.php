24e061f04fbe9d0a85ee8bfbfa7f757c36594881
Lucas <lucas.dias@unyleya.com.br>
2020-01-28 20:54:04 -0300

    Ajustes no recurso de Patrocinadores Adicionando métodos de alteração e remoção.

--- App/Business/Patrocinador.php
+++ App/Business/Patrocinador.php
@@ -4,8 +4,12 @@
 namespace App\Business;
 
 
+use App\Constants\TipoArquivo;
 use App\Model\Entity\Patrocinadorimagem;
+use App\Model\File;
+use App\Model\Validate;
 use Exception;
+use Gumlet\ImageResizeException;
 
 class Patrocinador
 {
@@ -81,6 +85,88 @@ class Patrocinador
         }
 
         return $patrocinadores;
+    }
+
+    /**
+     * @param $parametros
+     * @return \App\Model\Entity\Patrocinador|array
+     * @throws Exception
+     */
+    public function insert($parametros)
+    {
+        $patrocinadorEntity = new \App\Model\Entity\Patrocinador();
+        $patrocinadorEntity->validate(Validate::PATROCINADOR, [], $parametros, false);
+        $patrocinadorEntity->mount($parametros);
+        return $patrocinadorEntity->save();
+    }
+
+    /**
+     * @param $parametros
+     * @return \App\Model\Entity\Patrocinador|array
+     * @throws Exception
+     */
+    public function update($parametros)
+    {
+        $patrocinadorEntity = new \App\Model\Entity\Patrocinador();
+        $patrocinadorEntity->validate(Validate::PATROCINADOR, ["UPDATE"], $parametros, true);
+        $patrocinadorEntity->mount($parametros);
+        $patrocinadorEntity->save();
+        return $patrocinadorEntity;
+    }
+
+    /**
+     * @param $arquivo
+     * @param $id_patrocinador
+     * @return bool
+     * @throws ImageResizeException|Exception
+     */
+    public function vinculaImagem($arquivo, $id_patrocinador)
+    {
+        $file = new File();
+        $file->upload("Imagens/Patrocinador", $arquivo, TipoArquivo::TIPO_IMAGEM_DEFAULT);
+
+        $imagemEntiy = new \App\Model\Entity\Imagem();
+        $imagemEntiy->setStNome($file->getNome());
+        $imagemEntiy->setStPrefixotamanho(\App\Constants\Imagem::PREFIXO_ORIGINAL);
+        $imagemEntiy->setStUrl($file->getUrlAcesso());
+        $imagemEntiy->insert();
+
+        $imagemPatrocinador = new Patrocinadorimagem();
+        $imagemPatrocinador->setIdImagem($imagemEntiy->getIdImagem());
+        $imagemPatrocinador->setIdPatrocinador($id_patrocinador);
+        $imagemPatrocinador->insert();
+
+        $imagens = Imagem::resizeAndSave($imagemEntiy->getStUrl(), $file->getNome(), \App\Constants\Imagem::RESIZE, $file->getPathSave());
+
+        foreach ($imagens as $image) {
+            $imagemPatrocinador->clearObject();
+            $imagemPatrocinador->setIdPatrocinador($id_patrocinador);
+            $imagemPatrocinador->setIdImagem($image->getIdImagem());
+            $imagemPatrocinador->insert();
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $id_patrocinador
+     * @return bool
+     * @throws Exception
+     */
+    public function desvinculaImagens($id_patrocinador)
+    {
+        $patrocinadorImagem = new Patrocinadorimagem();
+        $patrocinadorImagem->setIdPatrocinador($id_patrocinador);
+        $imagens = $patrocinadorImagem->find();
+
+        foreach ($imagens as $imagem) {
+            $patrocinadorImagem->clearObject();
+            $patrocinadorImagem->mount($imagem);
+            $patrocinadorImagem->delete();
+            Imagem::deleteImage($imagem["id_imagem"]);
+        }
+
+        return true;
     }
 
 }
--- App/Controller/Api/PatrocinadorController.php
+++ App/Controller/Api/PatrocinadorController.php
@@ -5,28 +5,85 @@ namespace App\Controller\Api;
 
 
 use App\Business\Patrocinador;
+use App\Controller\Controller;
+use Exception;
 
-class PatrocinadorController
+class PatrocinadorController extends Controller
 {
 
+    /**
+     * @return \App\Model\Entity\Patrocinador|array|void
+     * @throws Exception
+     */
+    public function postAction()
+    {
+        try {
+
+            $input = $this->request->getAllParameters();
+
+            if ($input["id_patrocinador"]) {
+                return $this->putAction();
+            }
+
+            $this->getModel()->beginTransaction();
+            $arquivo = $this->request->getFile("st_file", false);
+
+            if (!$arquivo) {
+                throw new Exception("É obrigatório o envio da logo da empresa.");
+            }
+
+            $patrocinador = new Patrocinador();
+            $patrocinadorEntity = $patrocinador->insert($input);
+
+            if ($arquivo) {
+                $patrocinador->vinculaImagem($arquivo, $patrocinadorEntity->getIdPatrocinador());
+            }
+
+            $this->getModel()->commit();
+            return $patrocinadorEntity;
+
+        } catch (Exception $e) {
+            $this->getModel()->rollBack();
+            throw $e;
+        }
+
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
     public function getPatrocinadoresAction()
     {
         $patrocinador = new Patrocinador();
         return $patrocinador->getPatrocinadoresPorTipo(\App\Constants\Patrocinador::PATROCINADOR);
     }
 
+    /**
+     * @return mixed
+     * @throws Exception
+     */
     public function getRealizadoresAction()
     {
         $patrocinador = new Patrocinador();
         return $patrocinador->getPatrocinadoresPorTipo(\App\Constants\Patrocinador::REALIZADOR);
     }
 
+    /**
+     * @return mixed
+     * @throws Exception
+     */
     public function getApoiadoresAction()
     {
         $patrocinador = new Patrocinador();
         return $patrocinador->getPatrocinadoresPorTipo(\App\Constants\Patrocinador::APOIADOR);
     }
 
+    /**
+     * @param $id_patrocinador
+     * @return \App\Model\Entity\Patrocinador|mixed|void
+     * @throws Exception
+     */
     public function getAction($id_patrocinador)
     {
         $patrocinador = new Patrocinador();
@@ -34,9 +91,59 @@ class PatrocinadorController
         if (!empty($id_patrocinador)) {
             return $patrocinador->getOne($id_patrocinador);
         }
-
         return $patrocinador->getAll();
-
     }
 
+    /**
+     * @return \App\Model\Entity\Patrocinador|array|bool|void
+     * @throws Exception
+     */
+    public function putAction()
+    {
+        try {
+            $this->getModel()->beginTransaction();
+
+            $input = $this->request->getAllParameters();
+            $arquivo = $this->request->getFile("st_file", false);
+
+            $patrocinador = new Patrocinador();
+            $patrocinadorEntity = $patrocinador->update($input);
+
+            if ($arquivo) {
+                $patrocinador->desvinculaImagens($patrocinadorEntity->getIdPatrocinador());
+                $patrocinador->vinculaImagem($arquivo, $patrocinadorEntity->getIdPatrocinador());
+            }
+
+            $this->getModel()->commit();
+            return $patrocinadorEntity;
+        } catch (Exception $e) {
+            $this->getModel()->rollBack();
+            throw  $e;
+        }
+    }
+
+    /**
+     * @param $id
+     * @return bool|void
+     * @throws Exception
+     */
+    public function deleteAction($id)
+    {
+        try {
+            $this->getModel()->beginTransaction();
+
+            $patrocinador = new Patrocinador();
+            $patrocinador->desvinculaImagens($id);
+
+            $patrocinadorEntity = new \App\Model\Entity\Patrocinador();
+            $patrocinadorEntity->delete($id);
+
+            $this->getModel()->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->getModel()->rollBack();
+            throw $e;
+        }
+
+    }
 }
