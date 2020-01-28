33afba5b0054a1031bab2b0997401bb2fe871c28
Lucas <lucas.dias@unyleya.com.br>
2020-01-28 20:57:35 -0300

    Adição de recursos de Duvidas Remoção, inserção e alteração.

--- App/Controller/Api/DuvidaController.php
+++ App/Controller/Api/DuvidaController.php
@@ -4,10 +4,13 @@
 namespace App\Controller\Api;
 
 
+use App\Controller\Controller;
 use App\Model\Entity\Duvida;
+use App\Model\Response;
+use App\Model\Validate;
 use Exception;
 
-class DuvidaController
+class DuvidaController extends Controller
 {
 
     /**
@@ -23,7 +26,55 @@ class DuvidaController
             return $duvida->findOne($id_duvida);
         }
 
-        return $duvida->findAll();
+        return $duvida->findCustom("*", null, false, ["nu_order" => "ASC"]);
     }
+
+    /**
+     * @return Duvida|array|void
+     * @throws Exception
+     */
+    public function postAction()
+    {
+        $input = $this->request->getAllParameters();
+
+        if ($input["id_duvida"]) {
+            return $this->putAction();
+        }
+
+        $duvidaEntity = new Duvida();
+        $duvidaEntity->validate(Validate::DUVIDA, [], $input, false);
+        $duvidaEntity->mount($input);
+        return $duvidaEntity->insert();
+    }
+
+    /**
+     * @return array|void
+     * @throws Exception
+     */
+    public function putAction()
+    {
+        $input = $this->request->getAllParameters();
+        $duvidaEntity = new Duvida();
+        $duvidaEntity->validate(Validate::DUVIDA, ["UPDATE"], $input, true);
+        $duvidaEntity->mount($input);
+        return $duvidaEntity->update();
+    }
+
+    /**
+     * @param $id
+     * @return bool|void
+     * @throws Exception
+     */
+    public function deleteAction($id)
+    {
+        if (!$id) {
+            Response::failResponse("O identificador da dúvida não foi enviado.");
+        }
+
+        $duvidaEntity = new Duvida();
+        $duvidaEntity->delete($id);
+        return true;
+    }
+
 
 }
