27035475879b8c673311d0ca285e6387f7ca974e
Lucas <lucas.dias@unyleya.com.br>
2020-02-01 10:59:59 -0300

    Removendo função não utilizada. Adição de suporte a geração de uma única tabela

--- App/Controller/Api/EntityController.php
+++ App/Controller/Api/EntityController.php
@@ -4,16 +4,7 @@
 namespace App\Controller\Api;
 
 use App\Controller\Controller;
-use App\Model\Entity\Comentario;
-use App\Model\Entity\Comentarioimagem;
 use App\Model\Entity\EntityGenerate;
-use App\Model\Entity\Fotogaleria;
-use App\Model\Entity\Fotogaleriaimagem;
-use App\Model\Entity\Imagem;
-use App\Model\Entity\Jogo;
-use App\Model\Entity\Jogoimagem;
-use App\Model\Entity\Patrocinador;
-use App\Model\Entity\Patrocinadorimagem;
 use App\Model\Response;
 use Exception;
 
@@ -29,44 +20,18 @@ class EntityController extends Controller
     }
 
     /**
-     * @return array|bool
+     * @param null $table
+     * @return array|bool|void
      */
-    public function getAction()
+    public function getAction($table = null)
     {
         try {
             $entityg = new EntityGenerate();
-            return $entityg->generateEntity();
+            return $entityg->generateEntity($table);
         } catch (Exception $e) {
             Response::exceptionResponse($e);
             return true;
         }
-
-    }
-
-    public function corrigirImagensAction()
-    {
-        $jogoEnt = new Fotogaleria();
-        $jogos = $jogoEnt->findAll();
-
-        foreach ($jogos as $jogo) {
-            $jogoEnt->mount($jogo);
-            $campeonatoimagem = new Fotogaleriaimagem();
-            $campeonatoimagem->setIdFotogaleria($jogoEnt->getIdFotogaleria());
-            $imagensjogo = $campeonatoimagem->find();
-
-            $i = 0;
-            $prefixo = ["ori", "default", "sm", "md", "lg"];
-            foreach ($imagensjogo as $imagemd) {
-                $imagem = new Imagem();
-                $imagem->mount($imagemd);
-                $imagem->findOne();
-                $imagem->setStPrefixotamanho($prefixo[$i]);
-                $imagem->update();
-                $i++;
-            }
-
-        }
-
 
     }
 
